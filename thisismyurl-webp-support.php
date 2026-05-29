fb4cd1f760bcfe1addb2d29c2796af98f18df875
thisismyurl <122108986+thisismyurl@users.noreply.github.com>
2026-05-29 07:34:36 -0400

    Release 1.6149.0734: bundle hardened GitHub updater as namespaced duplicated code

--- thisismyurl-webp-support.php
+++ thisismyurl-webp-support.php
@@ -3,7 +3,7 @@
  * Plugin Name:       This Is My URL - WEBP Support
  * Plugin URI:        https://thisismyurl.com/thisismyurl-webp-support/
  * Description:       Non-destructive WebP/AVIF conversion with backups, bulk processing, and one-click restoration.
- * Version:           1.6148.2110
+ * Version:           1.6149.0734
  * Author:            Christopher Ross
  * Author URI:        https://thisismyurl.com/
  * Requires at least: 6.0
